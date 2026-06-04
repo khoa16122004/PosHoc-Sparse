79e5d2e464850e0f5c87ffd87d3b1f177ed21b36
khoa16122004 <khoatr.work@gmail.com>
2026-06-04 19:57:17 +0700

    Update util.py

--- util.py
+++ util.py
@@ -116,7 +116,11 @@ def get_SIGLIP_model(
     bnb_config = BitsAndBytesConfig(load_in_4bit=True)
     model = AutoModel.from_pretrained(model_name, quantization_config=bnb_config, device_map="auto", attn_implementation="sdpa")
     processor = AutoProcessor.from_pretrained(model_name)
-    print(processor)
+    image_processor = processor.image_processor
+    tokenizer = processor.tokenizer
+    print(image_processor)
+    print(tokenizer)
+    
     raise
 
     
