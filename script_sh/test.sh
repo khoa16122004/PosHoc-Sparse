86c99cce46bef54f693fe629a2f0edef08e2e2a4
khoa16122004 <khoatr.work@gmail.com>
2026-06-04 12:54:17 +0700

    Update test.sh

--- script_sh/test.sh
+++ script_sh/test.sh
@@ -8,7 +8,7 @@
 #SBATCH --gres=mps:2 # Không khai báo GPU; --gres=mps:l40:2 (card L40); --gres=mps:a100:2 (card A100, ưu tiên các job dùng > 40GB vRAM)
 #SBATCH --mem=4G
 #SBATCH --time=72:00:00
-REQUIRED_VRAM=10000  # Quan trọng - Số vRAM cần dùng (để tìm GPU phù hợp)
+REQUIRED_VRAM=5000  # Quan trọng - Số vRAM cần dùng (để tìm GPU phù hợp)
 # =========================================================
 # CHUẨN BỊ MÔI TRƯỜNG
 # =========================================================
