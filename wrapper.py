bf91be627099a19780aa2833f19865045fb7a9e6
khoa16122004 <khoatr.work@gmail.com>
2026-06-06 13:44:39 +0700

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -121,8 +121,6 @@ class VisionViTModelWrapper(VisionModelWrapper):
         score = logits[:, class_id].sum()
         attentions = outputs.attentions
         cams = []
-        print(attentions.shape)
-        print(logits.shape)
         
         for attn in attentions:
             grad = torch.autograd.grad(score, attn, retain_graph=True)[0]  # B x num_heads x num_tokens x num_tokens
